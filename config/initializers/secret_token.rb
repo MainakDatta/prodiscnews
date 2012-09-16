7c395feaa0aa4aa1ed6a1d892d2d938fd24b7431
Mainak Datta <mail.mnak@gmail.com>
2012-09-16 03:39:40 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prodiscnews::Application.config.secret_token = '9dd6d276cee4a7f1dbd89d0156297771ded835a0f86196bad0b9646d80d5afe40e8df2785115291bfd7ff499c8e280febbd494f95dc6b5adf0d9bff86e4d156c'
