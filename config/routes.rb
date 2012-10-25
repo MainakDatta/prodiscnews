d90a3c0f48690ea48c42025711251eefaf99b416
Mainak Datta <mail.mnak@gmail.com>
2012-10-25 02:23:55 -0400

    Made homepage dynamic, will add bootstrap next

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Prodiscnews::Application.routes.draw do
+  get "pages/home"
+
   resources :posts
 
   resources :comments
@@ -54,7 +56,7 @@ Prodiscnews::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
---
+++ db/migrate/20121001072642_droparticles.rb
@@ -0,0 +1,9 @@
+class Droparticles < ActiveRecord::Migration
+  def up
+  	drop_table :articles 
+  end
+
+  def down
+  	raise ActiveRecord::IrreversibleMigration
+  end
+end
