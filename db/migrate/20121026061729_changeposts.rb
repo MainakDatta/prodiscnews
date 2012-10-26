00036b2d122dd8fccf7971fd5f1a6d346e3d0c12
Mainak Datta <mail.mnak@gmail.com>
2012-10-26 02:54:10 -0400

    fixed posts scaffold

---
+++ db/migrate/20121026061729_changeposts.rb
@@ -0,0 +1,9 @@
+class Changeposts < ActiveRecord::Migration
+  def up
+  	remove_column :posts, :date
+  end
+
+  def down
+  	add_column :posts, :date, :datetime
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,29 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121001073534) do
-
-  create_table "comments", :force => true do |t|
-    t.string   "poster"
-    t.text     "content"
-    t.datetime "date"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
+ActiveRecord::Schema.define(:version => 20121026062724) do
 
   create_table "posts", :force => true do |t|
     t.string   "author"
     t.string   "title"
     t.text     "content"
-    t.datetime "date"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  create_table "users", :force => true do |t|
-    t.string   "name"
-    t.string   "password"
-    t.string   "email"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
